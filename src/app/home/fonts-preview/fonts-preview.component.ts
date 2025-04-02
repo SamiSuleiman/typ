716de5a4cb06deec148600ce86010ac3b6ad33b6
AntennaTower <antennatower@proton.me>
2025-04-02 12:30:07 +0200

    wip: better font preview

--- src/app/home/fonts-preview/fonts-preview.component.ts
+++ src/app/home/fonts-preview/fonts-preview.component.ts
@@ -4,17 +4,29 @@ import { DEFAULT_EDITOR_CONTENT } from '../home.consts';
 
 @Component({
   template: `
-    <div class="flex flex-col gap-1 items-end">
-      <div>
-        <button class="btn btn-sm btn-outline" (click)="onResetContent()">
+    <div class="flex flex-col gap-1 items-center">
+      <div class="flex items-center gap-1">
+        <button class="btn btn-sm btn-outline" (click)="onReset()">
           Reset
         </button>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          [value]="$fontSize()"
+          class="range"
+          [(ngModel)]="$fontSize"
+        />
       </div>
-      <textarea
-        class="textarea w-full"
-        [rows]="45"
-        [(ngModel)]="$content"
-      ></textarea>
+      <div class="w-[80vw] flex gap-2 overflow-scroll">
+        <textarea
+          [style]="{ fontSize: this.$fontSize() + 'px' }"
+          class="textarea grow resize-none overflow max-h-[90vh]"
+          [rows]="45"
+          [value]="$content()"
+          [(ngModel)]="$content"
+        ></textarea>
+      </div>
     </div>
   `,
   imports: [FormsModule],
@@ -22,8 +34,10 @@ import { DEFAULT_EDITOR_CONTENT } from '../home.consts';
 })
 export class FontsPreviewComponent {
   protected readonly $content = model(DEFAULT_EDITOR_CONTENT);
+  protected readonly $fontSize = model(15);
 
-  protected onResetContent() {
+  protected onReset() {
     this.$content.set(DEFAULT_EDITOR_CONTENT);
+    this.$fontSize.set(15);
   }
 }
